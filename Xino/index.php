4a26f0de82089373739c19e9528031be771ee158
Agastyarathee <67199509+Agastyarathee@users.noreply.github.com>
2021-06-16 11:18:56 +0530

    rf

---
+++ Xino/index.php
@@ -0,0 +1,281 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans&family=Roboto&display=swap" rel="stylesheet">
+
+</head>
+<body>
+        
+
+
+
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a class="navbar-brand" href="#">Xino</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="">Events</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="">About Us</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="contactus.html">Contact Us</a>
+      </li>
+
+        
+      </li>
+     
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+
+
+<div id="demo" class="carousel slide" data-ride="carousel">
+  <ul class="carousel-indicators">
+    <li data-target="#demo" data-slide-to="0" class="active"></li>
+    <li data-target="#demo" data-slide-to="1"></li>
+    <li data-target="#demo" data-slide-to="2"></li>
+  </ul>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="images/teckd.jpg" alt="3" width="1100" height="500">
+      <div class="carousel-caption">
+        <h2>Welcome To Xino</h2>
+        
+        <p>We are just a bunch of teck nerds!</p>
+      </div>   
+    </div>
+    <div class="carousel-item">
+      <img src="images/4rt.jfif" alt="2" width="1080" height="667">
+      <div class="carousel-caption">
+        <h3>We code </h3>
+        <p>Proggramers</p>
+      </div>   
+    </div>
+    <div class="carousel-item">
+      <img src="images/test.jpg" alt="1" width="1100" height="500">
+      <div class="carousel-caption">
+        <h3>love teck</h3>
+        <p>just Proggramers</p>
+      </div>   
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#demo" data-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </a>
+  <a class="carousel-control-next" href="#demo" data-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </a>
+</div>
+
+<section class="my-5">
+<div class="py-5">
+
+  <h2 class="text-center">
+    About Us
+  </h2>
+</div>
+<div class="container-fluid">
+  <div class ="row">
+<div class="col-lg-6 col-md-6 col-12">
+  <img src= "images/teamgg.jpg" class="img-fluid aboutimg">
+</div>
+<div class="col-lg-6 col-md-6 col-12">
+  <h2>Xino</h2>
+  <p>XINO, the computer club of DPS Rohini comprises of die hard fanatics of technology whose passion for the technological science verges on borderline crazy. We comprise of 25+ brilliant minds, each posessing a unique perspective of analysing the world around us.
+
+    "When all think alike, then no one is thinking." We value different ideas, dreams and aspirations of our members and work our best to turn those dreams into reality. "It isn't the incompetent who destroy an organization. The incompetent never get in a position to destroy it. It is those who achieved something and want to rest upon their achievements who are forever clogging things up". We appreciate our achievements but never stop our crusade to acheive long lasting glory in the field of technological sciences. We have taken the competition scene in Delhi by storm, winning innummerable events but never letting it get in our way of learning.</p>
+  <a href="aboutus.html" class=" btn btn-success">Check More</a>
+  </div>
+</div>
+  
+</div>
+</section>
+
+
+<section class="my-5">
+  <div class="py-5"> 
+  <h2 class="text-center">Events</h2>
+</div>
+
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-lg-4 col-md-4 col-12">
+      <div class="card">
+        <img class="card-img-top" src="images/EXQUISITE.png" alt="Card image">
+        <div class="card-body">
+          <h4 class="card-title">EXQUISITE</h4>
+          <p class="card-text">An event for those who wish to be frontrunners in this generation of technology. The preliminary submissions will be judged based on the quality of the product and its functionality. The Top 6 teams will pitch their ideas to our judges.
+
+            ELIGIBILITY: OPEN
+            NUMBER OF PARTICIPANTS PER TEAM: 3
+            NUMBER OF TEAMS PER SCHOOl: 1.</p>
+          <a href="#" class="btn btn-primary">See Profile</a>
+        </div>
+      </div>
+      
+    </div>
+
+
+    <div class="col-lg-4 col-md-4 col-12">
+      <div class="card">
+        <img class="card-img-top" src="images/EXQUISITE.png" alt="Card image">
+        <div class="card-body">
+          <h4 class="card-title">EXQUISITE</h4>
+          <p class="card-text">An event for those who wish to be frontrunners in this generation of technology. The preliminary submissions will be judged based on the quality of the product and its functionality. The Top 6 teams will pitch their ideas to our judges.
+
+            ELIGIBILITY: OPEN
+            NUMBER OF PARTICIPANTS PER TEAM: 3
+            NUMBER OF TEAMS PER SCHOOl: 1.</p>
+          <a href="#" class="btn btn-primary">See Profile</a>
+        </div>
+      </div>
+      
+    </div>
+
+
+    <div class="col-lg-4 col-md-4 col-12">
+      <div class="card">
+        <img class="card-img-top" src="images/EXQUISITE.png" alt="Card image">
+        <div class="card-body">
+          <h4 class="card-title">EXQUISITE</h4>
+          <p class="card-text">An event for those who wish to be frontrunners in this generation of technology. The preliminary submissions will be judged based on the quality of the product and its functionality. The Top 6 teams will pitch their ideas to our judges.
+
+            ELIGIBILITY: OPEN
+            NUMBER OF PARTICIPANTS PER TEAM: 3
+            NUMBER OF TEAMS PER SCHOOl: 1.</p>
+          <a href="#" class="btn btn-primary">See Profile</a>
+        </div>
+      </div>
+      
+    </div>
+
+    
+  </div>
+
+
+</div>
+</section>
+
+
+
+
+<section class="my-5">
+  <div class="py-5"> 
+  <h2 class="text-center">Our Achievements</h2>
+</div>
+<div class="container-fluid">
+  <div class="row">
+    <div class= "col-lg-4 col-md-4 col-12">
+      <img src="images/rtt.jpg" class="image-fluid pb-4" alt="victory">
+
+
+    </div>
+    <div class= "col-lg-4 col-md-4 col-12">
+      <img src="images/rtt.jpg" class="image-fluid pb-4" alt="victory">
+
+
+    </div>
+    <div class= "col-lg-4 col-md-4 col-12">
+      <img src="images/rtt.jpg" class="image-fluid pb-4" alt="victory">
+
+
+    </div>
+    <div class= "col-lg-4 col-md-4 col-12">
+      <img src="images/rtt.jpg" class="image-fluid pb-4" alt="victory">
+
+
+    </div>
+    <div class= "col-lg-4 col-md-4 col-12">
+      <img src="images/rtt.jpg" class="image-fluid pb-4" alt="victory">
+
+
+    </div>
+    <div class= "col-lg-4 col-md-4 col-12">
+      <img src="images/rtt.jpg" class="image-fluid pb-4" alt="victory">
+
+
+    </div>
+    <div class= "col-lg-4 col-md-4 col-12">
+      <img src="images/rtt.jpg" class="image-fluid pb-4" alt="victory">
+
+
+    </div>
+    <div class= "col-lg-4 col-md-4 col-12">
+      <img src="images/rtt.jpg" class="image-fluid pb-4" alt="victory">
+
+
+    </div>
+    <div class= "col-lg-4 col-md-4 col-12">
+      <img src="images/rtt.jpg" class="image-fluid pb-4" >
+
+
+    </div>
+   
+  </div>
+
+</div>
+</section>
+
+<section class="my-5">
+  <div class="py-5"> 
+  <h2 class="text-center">Contact Us</h2>
+</div>
+<div class="w-50 m-auto">
+  <form action="userinfo.php" method="POST">
+<div class="form-group">
+  <label>Username</label>
+  <input type="text" name="user" autocomplete="off" class="form-control">
+  
+</div>
+<div class="form-group">
+  <label>Email ID</label>
+  <input type="text" name="email" autocomplete="off" class="form-control">
+  
+</div>
+<div class="form-group">
+  <label>Mobile Number</label>
+  <input type="text" name="mobile" autocomplete="off" class="form-control">
+
+  <div class="form-group">
+    <label>Comment</label>
+    <textarea class="form-control"  name ="comment"></textarea>
+    
+    
+    
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</div>
+  </form>
+</div>
+</section>
+
+
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+
+</body>
+</html>
